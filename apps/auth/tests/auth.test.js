6455b255e5a0cc76c3bb732209b921047b81761a
Tushar57517 <sainitushar575@gmail.com>
2025-08-13 20:00:31 +0530

    add testcases for login controller

--- apps/auth/tests/auth.test.js
+++ apps/auth/tests/auth.test.js
@@ -1,5 +1,5 @@
 import request from "supertest";
-import { it, jest } from "@jest/globals";
+import { describe, it, jest } from "@jest/globals";
 
 jest.unstable_mockModule("../models/user.model.js", () => ({
   default: {
@@ -9,17 +9,24 @@ jest.unstable_mockModule("../models/user.model.js", () => ({
 }));
 
 jest.unstable_mockModule("bcrypt", () => ({
-  default: { hash: jest.fn() },
+  default: { hash: jest.fn(), compare: jest.fn() },
 }));
 
 jest.unstable_mockModule("../queues/userQueue.js", () => ({
   default: { add: jest.fn().mockResolvedValue(true) },
 }));
 
+jest.unstable_mockModule("jsonwebtoken", () => ({
+  default: {
+    sign: jest.fn().mockReturnValue("fake-token"),
+  },
+}));
+
 const bcrypt = (await import("bcrypt")).default;
 const app = (await import("../app.js")).default;
 const User = (await import("../models/user.model.js")).default;
 const userQueue = (await import("../queues/userQueue.js")).default;
+const jwt = (await import("jsonwebtoken")).default;
 
 describe("POST api/auth/register", () => {
   beforeEach(() => {
@@ -70,5 +77,76 @@ describe("POST api/auth/register", () => {
       userId: "user123",
       email: payload.email,
     });
+  });
+});
+
+describe("POST api/auth/login", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("returns 400 when email or password is empty", async () => {
+    const res = await request(app).post("/api/auth/login").send({
+      email: "  ",
+      password: "   ",
+    });
+
+    expect(res.status).toBe(400);
+    expect(res.body).toEqual({ error: "all fields required" });
+  });
+
+  it("returns 404 when user doesn't exists", async () => {
+    const payload = { email: "test@email.com", password: "1234" };
+
+    User.findOne.mockResolvedValueOnce(null);
+
+    const res = await request(app).post("/api/auth/login").send(payload);
+
+    expect(res.status).toBe(404);
+    expect(res.body).toStrictEqual({ error: "user not found" });
+  });
+
+  it("returns 403 when password doesn't match", async () => {
+    const payload = { email: "test@email.com", password: "1234" };
+
+    User.findOne.mockResolvedValue({
+      _id: "user123",
+      email: payload.email,
+      password: "hashed-password",
+    });
+
+    const invalidPayload = { email: "test@email.com", password: "1233" };
+
+    bcrypt.compare.mockResolvedValue(false);
+
+    const res = await request(app).post("/api/auth/login").send(invalidPayload);
+
+    expect(res.status).toBe(403);
+    expect(res.body).toEqual({ error: "password doesn't match" });
+  });
+
+  it("returns 200 with a token", async () => {
+    const payload = { email: "test@email.com", password: "1234" };
+
+    User.findOne.mockResolvedValue({
+      _id: "user123",
+      email: payload.email,
+      password: "hashed-password",
+    });
+
+    bcrypt.compare.mockResolvedValue(true);
+
+    jwt.sign.mockReturnValue("fake-token");
+
+    const res = await request(app).post("/api/auth/login").send(payload);
+
+    expect(res.status).toBe(200);
+    expect(res.body).toStrictEqual({ token: "fake-token" });
+
+    expect(jwt.sign).toHaveBeenCalledWith(
+      { userId: "user123" },
+      process.env.JWT_SECRET,
+      { expiresIn: process.env.JWT_EXPIRES_IN }
+    );
   });
 });
